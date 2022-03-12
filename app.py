4cd824f1f0eafa1a1ab470278bae11d2354b9d93
Athirson Silva <Athirsonarceus@gmail.com>
2022-03-12 14:05:44 -0300

    Project finished

---
+++ app.py
@@ -0,0 +1,48 @@
+import PySimpleGUI as sg
+
+# Create windows and layout
+def login_window():
+    layout = [
+        [sg.Text('Name')], 
+        [sg.Input()], 
+        [sg.Button('Next')], 
+    ]
+
+    return sg.Window('Login', layout = layout, finalize = True)
+
+def window_request():
+    sg.theme('Dark')
+    layout = [
+        [sg.Text('Send request')],
+        [sg.Checkbox('Pizza Pepperoni', key = 'pizza1'),
+        sg.Checkbox('Pizza portuguese', key = 'pizza2')],
+        [sg.Button('Back'), sg.Button('Send request')],
+    ]
+
+    return sg.Window('Request', layout = layout, finalize = True)
+
+# Create initials windows
+window1, window2 = login_window(), None
+
+# Create a loop to read events
+while True:
+    window, event, values = sg.read_all_windows()
+    # Window closed
+    if (window == window1 or window == window2) and event == sg.WIN_CLOSED:
+        break 
+    # Next window
+    if window == window1 and event == 'Next':
+        window1.hide()
+        window2 = window_request()
+    # Previous window
+    if window == window2 and event == 'Back':
+        window2.hide()
+        window1.un_hide()
+# Events listeners actions
+    if window == window2 and event == 'Send request':
+        if values['pizza1'] == True and values['pizza2'] == True:
+            sg.popup('Were requested a pepperoni and portuguese pizza')
+        elif values['pizza1'] == True:
+            sg.popup('Was requested a pepperoni pizza')
+        elif values['pizza2'] == True:
+            sg.popup('Was requested a portuguese pizza')
